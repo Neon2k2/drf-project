088ac02c5b5823d0009a3e41b6600325266d0d16
neon2k2 <gaurishninnekar@gmail.com>
2023-06-17 02:56:46 +0530

    updated function based view to class based view

--- watchbizz/myapp/api/urls.py
+++ watchbizz/myapp/api/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path, include
-from myapp.api.views import movie_details, movie_list
-
+# from myapp.api.views import movie_details, movie_list
+from myapp.api.views import MovieDetailAV, MovieListAV
 urlpatterns = [
 
-    path('list/', movie_list, name='movie_list'),
-    path('<int:pk>', movie_details, name='movie_details'),
+    path('list/', MovieListAV.as_view(), name='movie_list'),
+    path('<int:pk>', MovieDetailAV.as_view(), name='movie_details'),
 ]
